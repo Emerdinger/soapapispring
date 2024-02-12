41c553ef23912d5ea3fb847b56d14cf9ef148ad5
Emerson Niño <servicesemer@gmail.com>
2024-02-12 10:50:23 -0500

    Soap api with Spring Boot and Java17 first commit, Save Producs, Get All Products, Get Product By Name and Get Product By ID

---
+++ src/main/java/com/example/demosoap/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.demosoap.repository;
+
+import com.example.demosoap.model.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, Integer> {
+    ProductModel findByName(String name);
+    ProductModel findById(int id);
+}
